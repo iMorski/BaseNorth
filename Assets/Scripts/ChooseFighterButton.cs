16d19d56af13d879c392c1ccf647e41911f210b9
Ilia Muratov <i.murato@mail.ru>
2020-11-10 10:30:13 +0400

    Change selection

---
+++ Assets/Scripts/ChooseFighterButton.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using UnityEngine;
+
+public class ChooseFighterButton : MonoBehaviour
+{
+    [SerializeField] private GameObject Character;
+
+    private Animator Animator;
+
+    private void Awake()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    private void Start()
+    {
+        FB.ConnectionStepChange += OnConnectionStepChange;
+    }
+
+    private void OnConnectionStepChange()
+    {
+        if (!(FB.ConnectionStep != 5.0))
+        {
+            Animator.Play("ChooseFighterButton-Enable");
+        }
+    }
+
+    public void OnClick()
+    {
+        //foreach (Animator Animator in ChooseFighterButtonAnimator)
+        {
+            //if (!(Animator != OnClickAnimator))
+            {
+                //Animator.Play("Summon-Disable");
+            }
+            //else
+            {
+                //Animator.Play("Summon-Enable");
+            }
+        }
+    }
+}
